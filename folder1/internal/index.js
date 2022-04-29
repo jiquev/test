2370a120ae46b9cd08aca7dc2158b271a585842b
CMS <jose.queved@gagilisium.com>
2022-04-30 00:07:00 +0200

    Update file from CMS

--- folder1/internal/index.js
+++ folder1/internal/index.js
@@ -1,1 +1,18 @@
-
+{
+	"redirectUrls": [],
+	"redirectDelay": 1,
+	"redirectTimeout": 0,
+	"logoutRedirectTimeout": 0,
+	"loadDomainDynamically": true,
+	"registerFirst": true,
+	"userTimeout": 0,
+	"apple": {
+		"clientId": "sdfg",
+		"scope": "sdfg"
+	},
+	"facebook": {
+		"appId": "sdfg",
+		"permissions": [],
+		"version": "sdfg"
+	}
+}
